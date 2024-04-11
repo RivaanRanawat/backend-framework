b9421a2153c455eb71ef4366ba91720fe6157d4b
Rivaan Ranawat <namanrivaan@gmail.com>
2024-04-11 16:28:20 -0400

    rearranging the order, adding more example of multiple route with one fn

--- lib/example.py
+++ lib/example.py
@@ -13,8 +13,8 @@ def hello(request, response, name):
     print(name)
     response.send('Content is different', 200, 'OK')
 
-
 @slow_api.get('/thisfortheboi')
+@slow_api.post('/thisfortheboi')
 def hello3(request, response):
-    print(request)
+    # print(request.body)
     response.send('Content is different 23232', 200, 'OK')
--- lib/request.py
+++ lib/request.py
@@ -7,14 +7,16 @@ class Request:
         for key, value in environ.items():
             setattr(self, key.replace(".", "_").lower(), value)
         
-        # extracting query
-        req_queries = self.query_string.split('&')
-        
-        for query in req_queries:
-            query_key, query_val = query.split('=')
-            self.queries[query_key] = query_val
-        
+
         # if you only want self.body to exist when
         # the request method is not GET
         if self.request_method != 'GET':
             self.body = environ['wsgi.input'].read().decode()
+        
+        # extracting query
+        if self.query_string:
+            req_queries = self.query_string.split('&')
+            
+            for query in req_queries:
+                query_key, query_val = query.split('=')
+                self.queries[query_key] = query_val
